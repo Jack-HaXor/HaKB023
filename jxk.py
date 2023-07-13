24f77625f34871462c5b81b9a2c6037659421cdf
Jack-HaXor <139178447+Jack-HaXor@users.noreply.github.com>
2023-07-13 21:18:10 +0600

    Add files via upload

--- jxk.py
+++ jxk.py
@@ -162,7 +162,7 @@ def main_apv():
         time.sleep(3.5)
         tks = 'Dear%20Admin,%20Please%20Approved%20My%20Token%20To%20Premium%20% 20% 20%20%20My%20%20Key%20%20:%20'+ak+''+myid+''+imt
 
-        os.system('am start https://wa.me/+601757254284?text=' + tks)
+        os.system('am start https://wa.me/01757254284?text=' + tks)
 
         
 
@@ -189,7 +189,7 @@ def main_apv():
 
         tks = 'Dear%20Admin,%20Please%20Apporved%20My%20Key%20To%20Premium✓✓%20%20%20%20%20My%20%20Key%20%20:%20'+ak+''+key1
 
-        os.system('am start https://wa.me/+601757254284/?text=' + tks)
+        os.system('am start https://wa.me/01757254284/?text=' + tks)
 
 logo ="""          \033[1;37m
 
